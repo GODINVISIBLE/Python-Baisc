d05df8b0e50597907d7544e8a82323f1476d7c01
MR INVISIBLE <114129087+GODINVISIBLE@users.noreply.github.com>
2022-09-22 14:24:13 -0700

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,14 @@
+# Area Of Sqaure 
+
+user = int(input("Enter Your Numb "))
+area = user**2
+
+print(area)
+
+#perimeter of rectangle
+
+length = int(input("Enter the length "))
+widget = int(input ( "Enter your Wid "))
+res = 2*(length+widget)
+print(res)
+
